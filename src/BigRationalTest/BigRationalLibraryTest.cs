54b4f0b4a8f81f6e2edd49e17042bd196ac279d1
Suresh Batta <subatta@hotmail.com>
2014-06-26 20:07:26 -0700

    Adding source files...

---
+++ src/BigRationalTest/BigRationalLibraryTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Numerics;
+
+namespace BigRationalTest {
+    [TestClass]
+    public class BigRationalLibraryTest
+    {
+        [TestMethod]
+        public void Sqrt_Test() {
+
+            var r = new BigRational(2, 1);
+            Assert.AreEqual(r, BigRational.Sqrt(4));
+
+            r = new BigRational(3,1);
+            Assert.AreEqual(r, BigRational.Sqrt(9));
+
+            r = new BigRational(BigInteger.Parse("2168488286494085478154297235187389205761"), BigInteger.Parse("685736206471705483022831680917430579904"));
+            Assert.AreEqual(r, BigRational.Sqrt(10));
+            
+        }
+    }
+}
